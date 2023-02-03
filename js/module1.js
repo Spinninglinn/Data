29ce31c495540dadcdadb2e0aec0013c7fbcaf8d
LinnAnette <linnanette.o@gmail.com>
2023-02-03 17:35:51 +0100

    prøver lage slette knapp

---
+++ js/module1.js
@@ -0,0 +1,20 @@
+
+// kilde: https://www.geeksforgeeks.org/how-to-remove-an-added-list-items-using-javascript/
+//function addItem() {
+  //  const aElement = document.getElementById("handleliste");
+  //  const vare = document.getElementById("vare");
+  //  const li = document.createElement("li");
+  //  li.setAttribute('li', vare.value);
+  //  li.appendChild(document.createTextNode(vare.value));
+  //  aElement.appendChild(li);
+
+//}
+
+//function removeItem() {
+   // const aElement = document.getElementById("handleliste");
+   // const vare = document.getElementById("vare");
+   // const item = document.getElementById(vare.value);
+   // aElement.removeChild(item);
+//}
+
+
